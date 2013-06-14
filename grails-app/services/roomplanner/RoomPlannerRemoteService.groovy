a19b7d72b561cf1cf1437637c4704e66788b4b48
Vlad <v-eliseev@yandex.ru>
2013-06-15 00:04:00 +0400

    Split into SOAP and Hessian services

---
+++ grails-app/services/roomplanner/RoomPlannerRemoteService.groovy
@@ -0,0 +1,32 @@
+package roomplanner
+
+import roomplanner.api.RoomCategory as RoomCategoryDto
+import roomplanner.api.Room as RoomDto
+import roomplanner.api.Reservation as ReservationDto
+import roomplanner.api.RoomAssignment as RoomAssignmentDto
+import roomplanner.api.Plan as PlanDto
+
+/**
+	Provides an entry point for Hessian protocol
+*/
+class RoomPlannerRemoteService implements roomplanner.api.RoomPlannerService {
+
+	static expose = ['hessian']
+
+	/**
+		Implements doPlan method
+	*/
+	@Override
+	PlanDto doPlan(
+		List<RoomCategoryDto> roomCategoriesDto, 
+		List<RoomDto> roomsDto, 
+		List<ReservationDto> reservationsDto, 
+		List<RoomAssignmentDto> roomAssignmentsDto
+	) {
+		PlanDto planDto = null
+
+
+		planDto
+	}
+
+}

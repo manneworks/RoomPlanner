0ce99cf8d533a79334492684a521cce3996795ba
evv <v-eliseev@yandex.ru>
2013-06-15 15:57:03 +0400

    fixes

--- src/groovy/roomplanner/RoomCategory.groovy
+++ src/groovy/roomplanner/RoomCategory.groovy
@@ -1,6 +1,25 @@
 package roomplanner
 
+import roomplanner.api.RoomCategory as RoomCategoryDto
+
 class RoomCategory {
 	Long id
-	String name
+
+	/**
+		Creates Domain object from DTO
+	*/
+	static RoomCategory fromDto(RoomCategoryDto dto) {
+		new RoomCategory( 
+			id: dto.id
+		) 
+	}
+
+	/**
+		Renders object to DTO
+	*/
+	RoomCategoryDto toDto() {
+		new RoomCategoryDto(
+			id: id
+		)
+	}
 }

38647ce2f15e6cd6498bfe86e8b40a8ba4f17655
evv <v-eliseev@yandex.ru>
2013-06-17 16:32:57 +0400

    Hessian call implemented

--- grails-app/services/roomplanner/RoomPlannerRemoteService.groovy
+++ grails-app/services/roomplanner/RoomPlannerRemoteService.groovy
@@ -1,15 +1,11 @@
 package roomplanner
 
-import roomplanner.api.RoomCategory as RoomCategoryDto
-import roomplanner.api.Room as RoomDto
-import roomplanner.api.Reservation as ReservationDto
-import roomplanner.api.RoomAssignment as RoomAssignmentDto
-import roomplanner.api.Plan as PlanDto
-
 /**
 	Provides an entry point for Hessian protocol
 */
 class RoomPlannerRemoteService implements roomplanner.api.IRoomPlannerService {
+
+	def grailsApplication
 
 	static expose = [ 'hessian' ]
 
@@ -17,11 +13,11 @@ class RoomPlannerRemoteService implements roomplanner.api.IRoomPlannerService {
 		Implements doPlan method
 	*/
 	@Override
-	PlanDto doPlan(
-		List<RoomCategoryDto> roomCategoriesDto, 
-		List<RoomDto> roomsDto, 
-		List<ReservationDto> reservationsDto, 
-		List<RoomAssignmentDto> roomAssignmentsDto
+	def doPlan(
+		def roomCategoriesDto, 
+		def roomsDto, 
+		def reservationsDto, 
+		def roomAssignmentsDto
 	) {
 
 		def (roomCategories, rooms, reservations, roomAssignments) = 

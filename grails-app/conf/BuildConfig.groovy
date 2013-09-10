74d43b890ff376ca67c7ad9c179e0777f0fdb3ff
evv <v-eliseev@yandex.ru>
2013-09-10 14:29:11 +0400

    Upgrade to Grails 2.3.0

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,12 +1,25 @@
-grails.project.work.dir = "../../work/RoomPlanner"
-
-grails.servlet.version = "2.5" // Change depending on target container compliance (2.5 or 3.0)
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "../../work/RoomPlanner"
 grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: false, //[maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
 
 def env = System.getProperty('grails.env')
 
@@ -14,6 +27,7 @@ if (env in ['development', 'test']) {
     grails.plugin.location.'roomplanner-api' = '../roomplanner-api'
 }
 
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -61,17 +75,18 @@ grails.project.dependency.resolution = {
         compile ":cxf:1.1.1"
         compile ":remoting:1.3"
 
-        runtime ":resources:1.2.RC2"
-        runtime ":database-migration:1.3.5"
+        runtime ":resources:1.2"
+        runtime ":database-migration:1.3.6"
 
-        runtime ":hibernate:$grailsVersion"
+        //runtime ":hibernate:3.6.10.1" 
+        runtime ":hibernate4:4.1.11.1"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
         //runtime ":cached-resources:1.0"
         //runtime ":yui-minify-resources:0.1.4"
 
-        build ":tomcat:$grailsVersion"
+        build ":tomcat:7.0.42"
 
         test(":spock:0.7") {
             exclude "spock-grails-support"

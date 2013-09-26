8aef9b227f76fd36a6f2896b665af0f175f5d023
evv <v-eliseev@yandex.ru>
2013-09-26 17:33:01 +0400

    Basic HTTP Authentication added

---
+++ grails-app/conf/filters/SecurityFilters.groovy
@@ -0,0 +1,41 @@
+package filters
+
+import roomplanner.SystemUser
+
+class SecurityFilters {  
+
+    def filters = {  
+    
+        basicAuth(controller:'admin', action:'*') {  
+            before = {  
+                try {
+                    def authString = request.getHeader('Authorization')   
+
+                    if (!authString) {
+                        response.status = 401
+                        response.addHeader("WWW-Authenticate", "Basic realm=\"Secured Area\"")
+                        return false
+                    }   
+
+                    def encodedPair = authString - 'Basic '  
+                    def decodedPair = new String(new sun.misc.BASE64Decoder().decodeBuffer(encodedPair))
+                    def credentials = decodedPair.split(':')  
+                    def user = SystemUser.findByUsernameAndPassword(credentials[0],credentials[1])  
+
+                    if (!user) {  
+                        log.error("User $credentials not known")
+                        response.status = 403
+                        return false
+                    }
+                    
+                    return true
+
+                } catch (Exception e) {
+                    log.error("Error processing Basic HTTP Authenication", e)
+                    response.status = 403
+                    return false
+                }                
+            }  
+        }  
+    }
+}

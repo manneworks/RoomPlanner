497bc5417535eb83661e28680ab0c83c76a29454
Vlad <v-eliseev@yandex.ru>
2013-10-02 21:04:42 +0400

    roomplanner api 0.5.22

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -119,7 +119,7 @@ grails.project.dependency.resolution = {
         //test ":build-test-data:2.0.5"
 
         if (env == 'jenkins' || env == 'prod') {
-           runtime "grails-roomplanner-api:grails-roomplanner-api:0.5.21"
+           runtime "grails-roomplanner-api:grails-roomplanner-api:0.5.22"
            runtime "grails-roombix-ui:grails-roombix-ui:0.1.5"
         }
 

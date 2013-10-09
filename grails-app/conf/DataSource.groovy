6f9d18ee8860a03487133981047f9bec4e9caaa3
Vlad <v-eliseev@yandex.ru>
2013-10-10 00:34:58 +0400

    add jenkins env datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -27,6 +27,12 @@ environments {
             url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
     }
+    jenkins {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+        }
+    }
     dbschema {
         dataSource {
             pooled = true
